e973187f2edde2a68152fbc91577d7511ee18e54
aliffaddly <aliffaddly1@gmail.com>
2025-11-30 19:31:54 +0700

    add filter by month dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
-from datetime import date
+from datetime import date, datetime
 import requests
 import plotly.express as px
+import calendar
+from dateutil.relativedelta import relativedelta
 
 # Get user id from URL params
 query_params = st.query_params
@@ -12,8 +14,6 @@ if isinstance(user_id, list):
 if user_id is None or user_id == "":
     st.error("No UID provided.", icon="🚨")
     st.stop()
-
-st.set_page_config(page_title="FynMate Dashboard", layout="wide")
 
 headers = {
         "x-api-key": st.secrets["API_KEY"],
@@ -50,6 +50,12 @@ def fetch_transactions_today():
         st.error(f"Failed to fetch data from API: {e}")
         return pd.DataFrame()
 
+st.set_page_config(page_title="FynMate Dashboard", layout="wide")
+st.title("FynMate Dashboard")
+st.subheader("Lihat dan analisis pengeluaran kamu di sini 💸")
+st.divider()
+
+
 df, total_expense, count = fetch_transactions()
 df = df[["id", "message", "payment_method", "amount", "category", "created_at"]]
 
@@ -57,15 +63,46 @@ df["amount"] = df["amount"].astype(int)
 df["amount_display"] = df["amount"].apply(lambda x: f"Rp {x:,.0f}").str.replace(',', '.')
 
 df["created_at"] = pd.to_datetime(df["created_at"], format="ISO8601")
-df["created_at"] = pd.to_datetime(df["created_at"]).dt.strftime("%d-%m-%Y %H:%M")
+df["created_at_display"] = pd.to_datetime(df["created_at"]).dt.strftime("%d-%m-%Y %H:%M")
 
-df_display = df.rename(columns={
+# === Dropdwon filter by month ===
+# Generate last 6 months
+now = datetime.now()
+last_months = []
+for i in range(0,6):
+    dt = now - relativedelta(months=i)
+    last_months.append((dt.year, dt.month))
+
+# month options for dropdown
+month_options = [
+    f"{calendar.month_name[m]} {y}" for (y, m) in last_months
+]
+
+# Set default to current month
+col1,col2 = st.columns([1,5])
+with col1:
+    default_label = f"{calendar.month_name[now.month]} {now.year}"
+    selected_label = st.selectbox("Filter berdasarkan bulan:",month_options, index=0)
+
+# Filter dataframe based on selected month
+selected_month_name, selected_year = selected_label.split(" ")
+selected_year = int(selected_year)
+selected_month = list(calendar.month_name).index(selected_month_name)
+df_filtered = df[
+    (df["created_at"].dt.month == selected_month) &
+    (df["created_at"].dt.year == selected_year)
+]
+
+# Monthly total expense
+monthly_expense = df_filtered["amount"].sum()
+
+df_display = df_filtered.rename(columns={
     # "id": "ID",
     "message": "Transaction",
     "payment_method": "Payment Method",
     "amount_display": "Amount",
     "category": "Category",
-    "created_at": "Created At"
+    "created_at_display": "Created At"
 })[[
     # "ID",
     "Transaction",
@@ -75,18 +112,16 @@ df_display = df.rename(columns={
     "Created At"
 ]]
 
-st.title("FynMate Dashboard")
-st.subheader("Lihat dan analisis pengeluaran kamu di sini 💸")
-
 # r = requests.get(st.secrets["API_URL"]+"/transactions", headers=headers)
 # data = r.json()
 # st.write("Raw data from Supabase:", data)
 
 # === Summary Metrics ===
 df_display.index = range(1, len(df_display) + 1)
+st.subheader(f"Pengeluaran Kamu di Bulan {selected_month_name} {selected_year}")
 st.dataframe(df_display)
 
-st.metric("Total pengeluaran bulan ini", f"Rp {total_expense:,.0f}".replace(',', '.'))
+st.metric(f"Total pengeluaran bulan {selected_month_name} {selected_year}", f"Rp {monthly_expense:,.0f}".replace(',', '.'))
 # === Pagination ===
 # items_per_page = 10
 # if "page" not in st.session_state:
@@ -114,6 +149,21 @@ st.metric("Total pengeluaran bulan ini", f"Rp {total_expense:,.0f}".replace(',',
 
 st.divider()
 
+# #  === Chart Pengeluaran per Kategori ===
+st.subheader(f"Pengeluaran per Kategori {selected_month_name} {selected_year}")
+
+chart = (df_filtered.groupby('category')['amount'].sum().sort_values(ascending=False).reset_index())
+
+fig_bar = px.bar(chart, x='category', y='amount', labels={'x': 'Category', 'y': 'Amount'})
+fig_pie = px.pie(chart, names='category', values='amount')
+
+cola,colb = st.columns(2)
+with cola:
+    st.plotly_chart(fig_bar, use_container_width=True)
+with colb:
+    st.plotly_chart(fig_pie, use_container_width=True)
+
+st.divider()
 # #  === Summary pengeluaran hari ini ===
 st.subheader(f"Pengeluaran Hari Ini ({date.today().strftime('%d %b %Y')})")
 
@@ -150,14 +200,6 @@ if isinstance(result, tuple) and len(result) == 3:
 
 st.divider()
 
-# #  === Chart Pengeluaran per Kategori ===
-st.subheader("Pengeluaran per Kategori")
-chart = (df.groupby('category')['amount'].sum().sort_values(ascending=False).reset_index())
 
-fig_bar = px.bar(chart, x='category', y='amount', labels={'x': 'Category', 'y': 'Amount'})
-# fig_pie = px.pie(chart, names='category', values='amount', title='Proporsi Pengeluaran per Kategori')
-
-st.plotly_chart(fig_bar, use_container_width=True)
-# st.plotly_chart(fig_pie, use_container_width=True)
 
 st.warning("More to come! Please stay tuned", icon="😁")
